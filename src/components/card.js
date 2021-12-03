1d8c2e649c1d2ada1091714c92cc4ebf6c4d6738
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2021-12-03 16:39:53 -0700

    finished task 5

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,35 @@
+import axios from "axios"
+
+
 const Card = (article) => {
+
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imgContainer = document.createElement('div')
+  const img = document.createElement('img')
+  const authorName = document.createElement('span')
+
+        card.appendChild(headline)
+        card.appendChild(author)
+        author.appendChild(imgContainer)
+        author.appendChild(authorName)
+        imgContainer.appendChild(img)
+  
+        card.classList.add('card')
+        headline.classList.add('headline')
+        author.classList.add('author')
+        imgContainer.classList.add('img-container')
+
+
+        headline.textContent = article.headline
+        img.src = article.authorPhoto
+        authorName.textContent = article.authorName
+
+        card.addEventListener('click', () => {
+          console.log(article.headline)
+        })
+
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,9 +48,23 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  return card;
 }
 
 const cardAppender = (selector) => {
+  axios.get(`http://localhost:5000/api/articles`)
+  .then(resp => {
+    console.log(resp)
+    const { articles } = resp.data
+  Object.keys(articles).forEach( article =>{
+    console.log(articles[article])
+    
+  })
+   
+    // 
+    
+  })
+  
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
